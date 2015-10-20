3ec5b4bdfc9043c44f65432c34ab2cdf4d07ddae
erichcl <erich.c.lersch@gmail.com>
2015-10-19 23:31:47 -0200

    ROTAS
    Agora é possível traçar rotas com a biblioteca utilizada.
    Exemplo no método TracarRotas()

--- app/src/main/java/cc/tcc/elmat_2/MainActivity.java
+++ app/src/main/java/cc/tcc/elmat_2/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -11,29 +12,165 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements RoutingListener {
+
+    protected GoogleMap map;
+    protected LocationManager locationManager;
+    protected long gpsCheckTime = 60000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_svc);
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        map = mapFragment.getMap();
+        startLocationManager();
+
+        Location local = getBestLocation();
+        //CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(local.getLatitude(), local.getLongitude()));
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(18.015365, -77.499382));
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
+
+        map.moveCamera(center);
+        map.animateCamera(zoom);
+
+    }
+
+    private void startLocationManager()
+    {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        try {
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 5000, 0,
+                    new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+//                            CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude()));
+//                            CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
+//
+//                            map.moveCamera(center);
+//                            map.animateCamera(zoom);
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+
+                        }
+                    });
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    3000, 0, new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+//                            CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude()));
+//                            CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
+//
+//                            map.moveCamera(center);
+//                            map.animateCamera(zoom);
+
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+
+                        }
+                    });
+        }
+        catch(SecurityException ex){
+            Log.d("startLocationManager", "Security Issue: "+ex.getMessage() );
+        }
+
+    }
+
+    private Location getBestLocation() {
+        try
+        {
+            String TAG = "getBestLocation";
+            Location gpslocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location networkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            // if we have only one location available, the choice is easy
+            if (gpslocation == null) {
+                Log.d(TAG, "No GPS Location available.");
+                return networkLocation;
+            }
+            if (networkLocation == null) {
+                Log.d(TAG, "No Network Location available");
+                return gpslocation;
+            }
+            // a locationupdate is considered 'old' if its older than the configured
+            // update interval. this means, we didn't get a
+            // update from this provider since the last check
+            long old = System.currentTimeMillis() -  gpsCheckTime;
+            boolean gpsIsOld = (gpslocation.getTime() < old);
+            boolean networkIsOld = (networkLocation.getTime() < old);
+            // gps is current and available, gps is better than network
+            if (!gpsIsOld) {
+                Log.d(TAG, "Returning current GPS Location");
+                return gpslocation;
+            }
+            // gps is old, we can't trust it. use network location
+            if (!networkIsOld) {
+                Log.d(TAG, "GPS is old, Network is current, returning network");
+                return networkLocation;
+            }
+            // both are old return the newer of those two
+            if (gpslocation.getTime() > networkLocation.getTime()) {
+                Log.d(TAG, "Both are old, returning gps(newer)");
+                return gpslocation;
+            } else {
+                Log.d(TAG, "Both are old, returning network(newer)");
+                return networkLocation;
+            }
+        }
+        catch(SecurityException ex)
+        {
+            Log.d("getBestLocation", "Security Issue: "+ex.getMessage() );
+        }
+        return null;
     }
 
 
@@ -53,35 +190,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            TesteRota();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onMapReady(GoogleMap map) {
-        map.setMyLocationEnabled(true);
-        map.getMyLocation();
-        try {
-            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            LocationListener locationListener = new MyLocationListener();
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-            // Add a marker in Sydney and move the camera
-            Location lastKnown = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            double lat = lastKnown.getLatitude();
-            double lon = lastKnown.getLongitude();
-            LatLng marker = new LatLng(lat, lon);
-            String name = getCityName(lastKnown);
-            map.addMarker(new MarkerOptions().position(marker).title("Marker in "+name));
-            map.moveCamera(CameraUpdateFactory.newLatLng(marker));
-        }
-        catch (SecurityException e)
-        {
+    private void TesteRota()
+    {
+        LatLng start = new LatLng(18.015365, -77.499382);
+        LatLng waypoint= new LatLng(18.01455, -77.499333);
+        LatLng end = new LatLng(18.012590, -77.500659);
 
-        }
+        Routing routing = new Routing.Builder()
+                .travelMode(Routing.TravelMode.WALKING)
+                .withListener(this)
+                .waypoints(start, waypoint, end)
+                .build();
+        routing.execute();
     }
-
 
     private String getCityName(Location location)
     {
@@ -101,28 +229,30 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return cityName;
     }
 
-    private class MyLocationListener implements LocationListener {
+    @Override
+    public void onRoutingFailure() {
 
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-
-        @Override
-        public void onLocationChanged(Location location) {
-
-        }
     }
+
+    @Override
+    public void onRoutingStart() {
+
+    }
+
+    @Override
+    public void onRoutingSuccess(PolylineOptions polylineOptions, Route route) {
+        PolylineOptions polyoptions = new PolylineOptions();
+        polyoptions.color(Color.BLUE);
+        polyoptions.width(10);
+        polyoptions.addAll(polylineOptions.getPoints());
+        map.addPolyline(polyoptions);
+    }
+
+    @Override
+    public void onRoutingCancelled() {
+
+    }
+
 
     @Override
     public void onBackPressed() {
